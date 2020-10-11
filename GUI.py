35512801240c7944b846daf0cba6cf812c9ee4f4
jyotirsai <jyotirsai28@gmail.com>
2020-10-10 20:52:44 -0600

    plot added to GUI

--- GUI.py
+++ GUI.py
@@ -1,6 +1,42 @@
-from PyQt5.QtWidgets import QApplication, QLabel
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from PyQt5 import QtCore, QtWidgets
+import sys
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime as dt
+from Datafetch import finalData
+matplotlib.use('Qt5Agg')
 
-app = QApplication([])
-label = QLabel("Hello World!")
-label.show()
+
+dateString = finalData[0]
+prices = finalData[1]
+dates = [dt.datetime.strptime(d, '%Y-%m-%d').date() for d in dateString]
+
+
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        # Create the maptlotlib FigureCanvas object,
+        # which defines a single set of axes as self.axes.
+        sc = MplCanvas()
+        sc.axes.plot(dates, prices)
+        self.setCentralWidget(sc)
+
+        self.show()
+
+
+app = QtWidgets.QApplication(sys.argv)
+w = MainWindow()
 app.exec_()
